0e038a2f7d7a68a0b9ea19bd3fabd385347d81dd
holynull <holynull@126.com>
2023-05-18 14:23:45 +0800

    bug

--- callback.py
+++ callback.py
@@ -100,7 +100,7 @@ class AgentCallbackHandler(BaseCallbackHandler):
     ) -> None:
         """Run on agent end."""
         print(f"ON_AGENT_FINISH: {finish.return_values}")
-        resp = ChatResponse(sender="bot", message=outputs.output, type="stream")
+        resp = ChatResponse(sender="bot", message=finish.return_values.output, type="stream")
         await self.websocket.send_json(resp.dict())
         
 
