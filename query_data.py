a793522de55d62264599c05c8e8e6e199374f29a
holynull <holynull@126.com>
2023-05-19 17:47:43 +0800

    11

--- query_data.py
+++ query_data.py
@@ -67,8 +67,8 @@ def get_agent(
         # request_timeout=120,
     ) 
     search = GoogleSerperAPIWrapper()
-    doc_search_swft = RetrievalQA.from_chain_type(llm=llm_qa, chain_type=chain_type, retriever=vcs_swft.as_retriever(),return_source_documents=True)
-    doc_search_path = RetrievalQA.from_chain_type(llm=llm_qa, chain_type=chain_type, retriever=vcs_path.as_retriever(),return_source_documents=True)
+    doc_search_swft = RetrievalQA.from_chain_type(llm=llm_qa, chain_type=chain_type, retriever=vcs_swft.as_retriever())
+    doc_search_path = RetrievalQA.from_chain_type(llm=llm_qa, chain_type=chain_type, retriever=vcs_path.as_retriever())
     # doc_search = get_qa_chain(chain_type=chain_type,vectorstore=vectorstore) 
     # zapier = ZapierNLAWrapper()
     # toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
