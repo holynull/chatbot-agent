7e9ea1fb2f3c89d123a90586cb4bf5e39d509d09
holynull <holynull@126.com>
2023-05-25 17:21:37 +0800

    modification of api doc

--- chain/all_templates.py
+++ chain/all_templates.py
@@ -115,6 +115,7 @@ convert:
 convert_id:
 	Type: string
     Optionally calculate market quotes by CoinMarketCap ID instead of symbol. This option is identical to convert outside of ID format. Ex: convert_id=1,2781 would replace convert=BTC,USD in your query. This parameter cannot be used when convert is used.
+aux: Optionally specify a comma-separated list of supplemental data fields to return. Pass num_market_pairs,cmc_rank,date_added,tags,platform,max_supply,circulating_supply,total_supply,market_cap_by_total_supply,volume_24h_reported,volume_7d,volume_7d_reported,volume_30d,volume_30d_reported,is_active,is_fiat to include all auxiliary fields.
 
 RESPONSE
 200 Successful
