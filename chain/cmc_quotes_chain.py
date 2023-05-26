24f480f5060509fefeef11a772e47fedef280701
holynull <holynull@126.com>
2023-05-26 17:59:01 +0800

    1

--- chain/cmc_quotes_chain.py
+++ chain/cmc_quotes_chain.py
@@ -157,7 +157,7 @@ class CMCQuotesChain(Chain):
         API_URL_PROMPT_TEMPLATE = """You are given the below API Documentation:
         {api_docs}
         Using this documentation, generate the full API url to call for answering the user question.
-        You should build the API url in order to get a response that is as short as possible, while still getting the necessary information to answer the question. Pay attention to deliberately exclude any unnecessary pieces of data in the API call.
+        You should build the API url in order to get a response that is as short as possible. Pay attention to deliberately exclude any unnecessary pieces of data in the API call.
         You should not build API url with the word "apikey".
         You should not build API url with the word "aux".
         Question:{question}
