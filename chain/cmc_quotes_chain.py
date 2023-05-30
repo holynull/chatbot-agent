80bd97c32f8dcbef7c99fd9c2492e61e4a79eece
holynull <holynull@126.com>
2023-05-30 15:32:20 +0800

    1

--- chain/cmc_quotes_chain.py
+++ chain/cmc_quotes_chain.py
@@ -157,7 +157,7 @@ class CMCQuotesChain(Chain):
             ],
             template=API_URL_PROMPT_TEMPLATE,
         )
-        api_llm=OpenAI(
+        api_llm=ChatOpenAI(
             # model_name="gpt-4",
             temperature=0,
             request_timeout=600,
